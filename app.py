e92361355b7bbdbd671b4687edee9d028df3e127
jorgecif <jorgecif@gmail.com>
2021-02-16 16:44:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def get_table_download_link_csv(df):
     """
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" target="_blank">Download csv file</a>'
     return href
 
 # Obtener tweets de usuarios y palabras entre 2 fechas
